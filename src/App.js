7b479ecd58bcae493e86a296a09cedd5dc580f8f
GorSharoyan <gor.sharoyan95@gmail.com>
2021-10-04 01:19:21 +0400

    :green_heart:CI(App.js):wrote basic functions to armenificate ,detect unabigous numbers and armenificate them

--- src/App.js
+++ src/App.js
@@ -5,9 +5,51 @@ import { useState } from "react";
 function App() {
   const [value, setValue] = useState("");
 
-  const handleInputChange = (value) => {
-    setValue(value.target.value);
+  //basic input change function
+  const handleInputChange = (val) => {
+    const input = val.target.value;
+    armenificate(input);
   };
+
+  //main armenification function
+  function armenificate(str) {
+    let result = "";
+    switch (str.length) {
+      case 1:
+        result += generateUnabigous(str);
+    }
+    return result;
+  }
+
+  //Ones function armenificatior
+  function generateUnabigous(input) {
+    const number = Number(input);
+    console.log("debugged");
+    console.log(number);
+
+    switch (input) {
+      case 0:
+        return "զրո";
+      case 1:
+        return "մեկ";
+      case 2:
+        return "երկու";
+      case 3:
+        return "երեք";
+      case 4:
+        return "չորս";
+      case 5:
+        return "հինգ";
+      case 6:
+        return "վեց";
+      case 7:
+        return "յոթ";
+      case 8:
+        return "ութ";
+      case 9:
+        return "ինը";
+    }
+  }
 
   return (
     <div>
@@ -15,7 +57,7 @@ function App() {
         <input type="number" onChange={handleInputChange}></input>
       </div>
       <div>
-        <p>{value}</p>
+        <h1>{value}</h1>
       </div>
     </div>
   );
