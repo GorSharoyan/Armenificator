3fd03bc91c998f12f4a86540da37f2de3da1ad34
Zen 0ne <gor.sharoyan95@gmail.com>
2021-10-12 21:49:06 -0700

    :green_heart:CI(App.js):created special function for unique two digit numbers

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   //basic input change function
   const handleInputChange = (val) => {
     const input = val.target.value;
-    armenificate(input);
+
+    setValue(armenificate(input));
   };
 
   //main armenification function
@@ -17,17 +18,43 @@ function App() {
     switch (str.length) {
       case 1:
         result += generateUnabigous(str);
+        break;
+      case 2:
+        result += genereateTwoDigits(str, result);
+        break;
+      case 3:
+        result += generateThreeDigit(str[0]);
+        result += " ";
+        result += generateCustomTwoDigit(str[1]);
+        result += generateUnabigous(str[2]);
+        break;
+      case 4:
+        result += generateFourDigit(str[0]);
+        result += " ";
+        result += generateThreeDigit(str[1]);
+        result += " ";
+        result += generateCustomTwoDigit(str[2]);
+        result += generateUnabigous(str[3]);
+        break;
+      case 5:
+        break;
     }
+
+    //Unique cases
+    // for (let i = 0; i < uniques.length; i++) {
+    //   if (result === uniques[i]) {
+    //     generateUniqueNumber(result);
+    //   } else {
+    //     return result;
+    //   }
+    // }
     return result;
   }
 
   //Ones function armenificatior
   function generateUnabigous(input) {
     const number = Number(input);
-    console.log("debugged");
-    console.log(number);
-
-    switch (input) {
+    switch (number) {
       case 0:
         return "զրո";
       case 1:
@@ -48,6 +75,91 @@ function App() {
         return "ութ";
       case 9:
         return "ինը";
+    }
+  }
+
+  //Twos function armenificator
+  function generateCustomTwoDigit(input) {
+    const number = Number(input);
+    switch (number) {
+      case 1:
+        return "տասն";
+      case 2:
+        return "քսան";
+      case 3:
+        return "երեսուն";
+      case 4:
+        return "քառասուն";
+      case 5:
+        return "հիսուն";
+      case 6:
+        return "վաթսուն";
+      case 7:
+        return "յոթանասուն";
+      case 8:
+        return "ութանասուն";
+      case 9:
+        return "իննսուն";
+    }
+  }
+
+  function genereateTwoDigits(input, result) {
+    let number = Number(input);
+    let uniques = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    for (let i = 0; i < uniques.length; i++) {
+      if (number === uniques[i]) {
+        return generateUniqueTwoDigit(input);
+      } else {
+        result += generateCustomTwoDigit(input[0]);
+        result += generateUnabigous(input[1]);
+        return result;
+      }
+    }
+  }
+
+  //Unique Twos number debug function
+  function generateUniqueTwoDigit(input) {
+    const number = Number(input);
+    switch (number) {
+      case 10:
+        return "տաս";
+      case 20:
+        return "քսան";
+      case 30:
+        return "երեսուն";
+      case 40:
+        return "քառասուն";
+      case 50:
+        return "հիսուն";
+      case 60:
+        return "վաթսուն";
+      case 70:
+        return "յոթանասուն";
+      case 80:
+        return "ութանասուն";
+      case 90:
+        return "իննսուն";
+    }
+  }
+
+  //Threes function armenificator
+  function generateThreeDigit(input) {
+    const number = Number(input);
+
+    if (number === 1) {
+      return "հարյուր ";
+    } else {
+      return generateUnabigous(number) + " " + "հարյուր ";
+    }
+  }
+  //Fours function armenificator
+  function generateFourDigit(input) {
+    const number = Number(input);
+
+    if (number === 1) {
+      return "հազար ";
+    } else {
+      return generateUnabigous(number) + " " + "հազար ";
     }
   }
 
