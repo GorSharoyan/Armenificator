94fdc9a86c152572e9652ae3fa51475d600a0307
Zen 0ne <gor.sharoyan95@gmail.com>
2021-10-17 23:49:24 +0400

    :package:NPM(MaterialUI):added materaial Ui core to the project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+//React Imports
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+//UI
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+//components
+import Navbar from "../Navbar/Navbar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Navbar />
+          <Typography variant="h6" className={classes.title}></Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
